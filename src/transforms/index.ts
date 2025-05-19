93447212d171d123aa3d941eb540da1fbceabe89
Zachary Stence <zachstence@gmail.com>
2025-05-19 13:26:36 -0500

    fix: disable table transform

--- src/transforms/index.ts
+++ src/transforms/index.ts
@@ -1,2 +1,5 @@
-import table from './table';
-export default [table];
+import type { Node } from '../types';
+// import table from './table';
+
+// Evidence uses a custom table tag and we dont want the default Markdoc transform
+export default [] as ((node: Node) => void)[];
